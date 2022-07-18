03556abd1181c1c9dc6962315d99b4808addded3
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2022-07-18 23:54:34 +0200

    Changed indentation from tabs to spaces

--- definitions.js
+++ definitions.js
@@ -1,225 +1,231 @@
+//Missing: lookaround groups, backreferences (/\1/)
+//Error when using lazy repeat quantifiers /a{3}?/
+//Need to wrap body of optionally quantifier to work properly
+//Not consistent
+
+
 /**
  * @typedef {Object<string, any>} ObjectLiteral
  */
 
 const RegexComponent = class RegexComponent {
-	constructor() { }
+    constructor() { }
 };
 
 RegexComponent.Constant = class extends RegexComponent {
-	static len = 1;
+    static len = 1;
 };
 
 RegexComponent.Block = class extends RegexComponent {
-	constructor(args = {}) {
-		super(args);
-		this.len = 0;
+    constructor(args = {}) {
+        super(args);
+        this.len = 0;
 
-		const {
-			body = []
-		} = args;
+        const {
+            body = []
+        } = args;
 
-		this.body = body;
-	}
+        this.body = body;
+    }
 
-	get bodyLength() {
-		return this.body.reduce((sum, e) => sum + (e instanceof RegexComponent.Block ? e.len + e.bodyLength : (e.length || e.len || Infinity)), 0);
-	}
+    get bodyLength() {
+        return this.body.reduce((sum, e) => sum + (e instanceof RegexComponent.Block ? e.len + e.bodyLength : (e.length || e.len || Infinity)), 0);
+    }
 
-	wrap(string) {
-		return this.body.length === 1 && this.bodyLength === 1 ? string : `(?:${string})`;
-	}
+    wrap(string) {
+        return this.body.length === 1 && this.bodyLength === 1 ? string : `(?:${string})`;
+    }
 
-	toArray() {
-		return this.body.map(component => {
-			if(typeof component === "string") return escapeRegexString(component);
-			if(component instanceof RegExp) return component.toString().replace(/^\/([\s\S]*?)\/[gmixsuXUAJD]*$/m, "$1");
-			return component.toString();
-		});
-	}
+    toArray() {
+        return this.body.map(component => {
+            if(typeof component === "string") return escapeRegexString(component);
+            if(component instanceof RegExp) return component.toString().replace(/^\/([\s\S]*?)\/[gmixsuXUAJD]*$/m, "$1");
+            return component.toString();
+        });
+    }
 };
 
 RegexComponent.Quantifier = class extends RegexComponent.Block {
-	constructor(args = {}) {
-		super(args);
-		this.len = 2;
+    constructor(args = {}) {
+        super(args);
+        this.len = 2;
 
-		const {
-			isLazy = false
-		} = args;
+        const {
+            isLazy = false
+        } = args;
 
-		this.isLazy = isLazy;
-	}
+        this.isLazy = isLazy;
+    }
 };
 
 /* Constants */
 const $whitespace = class extends RegexComponent.Constant {
-	static toString() {
-		return "\\s";
-	}
+    static toString() {
+        return "\\s";
+    }
 };
 
 const $digit = class extends RegexComponent.Constant {
-	static toString() {
-		return "\\d";
-	}
+    static toString() {
+        return "\\d";
+    }
 };
 
 const $word = class extends RegexComponent.Constant {
-	static toString() {
-		return "\\w";
-	}
+    static toString() {
+        return "\\w";
+    }
 };
 
 
 
 RegexComponent.Regex = class extends RegexComponent.Block {
-	constructor(args = {}) {
-		super(args);
+    constructor(args = {}) {
+        super(args);
 
-		this.flags = [];
-	}
-
-
-	/* Flags */
-
-	//This is custom made, the original does not support the "g" flag
-	returnAllMatches() {
-		this.flags.push("g");
-		return this;
-	}
-
-	anchorsMatchLineEndings() {
-		this.flags.push("m");
-		return this;
-	}
-
-	ignoresCase() {
-		this.flags.push("i");
-		return this;
-	}
-
-	dotMatchesNewlines() {
-		this.flags.push("s");
-		return this;
-	}
+        this.flags = [];
+    }
 
 
-	toString(includeFlags = false) {
-		const body = this.toArray().join("");
+    /* Flags */
 
-		return includeFlags ? `/${body}/${this.flags.join("")}` : body;
-	}
+    //This is custom made, the original does not support the "g" flag
+    returnAllMatches() {
+        this.flags.push("g");
+        return this;
+    }
 
-	build() {
-		return new RegExp(this.toString(), this.flags.join(""));
-	}
+    anchorsMatchLineEndings() {
+        this.flags.push("m");
+        return this;
+    }
+
+    ignoresCase() {
+        this.flags.push("i");
+        return this;
+    }
+
+    dotMatchesNewlines() {
+        this.flags.push("s");
+        return this;
+    }
+
+
+    toString(includeFlags = false) {
+        const body = this.toArray().join("");
+
+        return includeFlags ? `/${body}/${this.flags.join("")}` : body;
+    }
+
+    build() {
+        return new RegExp(this.toString(), this.flags.join(""));
+    }
 };
 
 RegexComponent.One = class extends RegexComponent.Block {
-	constructor(args = {}) {
-		super(args);
-	}
+    constructor(args = {}) {
+        super(args);
+    }
 
-	toString() {
-		return this.toArray().join("");
-	}
+    toString() {
+        return this.toArray().join("");
+    }
 };
 
 RegexComponent.Capture = class extends RegexComponent.Block {
-	constructor(args = {}) {
-		super(args);
-		this.len = 1;
+    constructor(args = {}) {
+        super(args);
+        this.len = 1;
 
-		const {
-			name
-		} = args;
+        const {
+            name
+        } = args;
 
-		this.name = name;
-	}
+        this.name = name;
+    }
 
-	toString() {
-		const body = this.toArray().join("");
+    toString() {
+        const body = this.toArray().join("");
 
-		return `(${this.name ? `?<${this.name}>` : ""}${body})`;
-	}
+        return `(${this.name ? `?<${this.name}>` : ""}${body})`;
+    }
 };
 
 RegexComponent.ChoiceOf = class extends RegexComponent.Block {
-	constructor(args = {}) {
-		super(args);
-	}
+    constructor(args = {}) {
+        super(args);
+    }
 
-	toString() {
-		return this.toArray().join("|");
-	}
+    toString() {
+        return this.toArray().join("|");
+    }
 };
 
 
 RegexComponent.OneOrMore = class extends RegexComponent.Quantifier {
-	constructor(args = {}) {
-		super(args);
-	}
+    constructor(args = {}) {
+        super(args);
+    }
 
-	toString() {
-		const body = this.toArray().join("");
+    toString() {
+        const body = this.toArray().join("");
 
-		return `${this.wrap(body)}+${this.isLazy ? "?" : ""}`;
-	}
+        return `${this.wrap(body)}+${this.isLazy ? "?" : ""}`;
+    }
 };
 
 RegexComponent.ZeroOrMore = class extends RegexComponent.Quantifier {
-	constructor(args = {}) {
-		super(args);
-	}
+    constructor(args = {}) {
+        super(args);
+    }
 
-	toString() {
-		const body = this.toArray().join("");
+    toString() {
+        const body = this.toArray().join("");
 
-		return `${this.wrap(body)}*${this.isLazy ? "?" : ""}`;
-	}
+        return `${this.wrap(body)}*${this.isLazy ? "?" : ""}`;
+    }
 };
 
 RegexComponent.Optionally = class extends RegexComponent.Quantifier {
-	constructor(args = {}) {
-		super(args);
-	}
+    constructor(args = {}) {
+        super(args);
+    }
 
-	toString() {
-		const body = this.toArray();
+    toString() {
+        const body = this.toArray();
 
-		// if(this.body.length > 1) {
-		// 	console.warn("Optionally quantifier should only contain one component");
-		// } else if(this.body[0] instanceof RegexComponent.Quantifier) {
-		// 	console.warn("Optionally quantifier should not contain a quantifier as first child");
-		// } else if(this.body.length == 1 && body[0].length > 1) {
-		// 	console.warn("Optionally quantifier should not contain a single component that is longer than one character");
-		// }
+        // if(this.body.length > 1) {
+        // 	console.warn("Optionally quantifier should only contain one component");
+        // } else if(this.body[0] instanceof RegexComponent.Quantifier) {
+        // 	console.warn("Optionally quantifier should not contain a quantifier as first child");
+        // } else if(this.body.length == 1 && body[0].length > 1) {
+        // 	console.warn("Optionally quantifier should not contain a single component that is longer than one character");
+        // }
 
-		return `${this.wrap(body.join(""))}?${this.isLazy ? "?" : ""}`;
-	}
+        return `${this.wrap(body.join(""))}?${this.isLazy ? "?" : ""}`;
+    }
 };
 
 RegexComponent.Repeat = class extends RegexComponent.Quantifier {
-	constructor(args = {}) {
-		super(args);
+    constructor(args = {}) {
+        super(args);
 
-		const {
-			from = 0,
-			to,
-			count
-		} = args;
+        const {
+            from = 0,
+            to,
+            count
+        } = args;
 
-		this.from = from;
-		this.to = to;
-		this.count = count;
-	}
+        this.from = from;
+        this.to = to;
+        this.count = count;
+    }
 
-	toString() {
-		const body = this.toArray().join("");
+    toString() {
+        const body = this.toArray().join("");
 
-		return `${this.wrap(body)}{${this.count || `${this.from},${this.to || ""}`}}${this.isLazy ? "?" : ""}`;
-	}
+        return `${this.wrap(body)}{${this.count || `${this.from},${this.to || ""}`}}${this.isLazy ? "?" : ""}`;
+    }
 };
 
 
@@ -229,58 +235,58 @@ RegexComponent.Repeat = class extends RegexComponent.Quantifier {
 /* Builder functions */
 
 function Regex() {
-	const {tree} = parseArgs(arguments);
+    const {tree} = parseArgs(arguments);
 
-	return new RegexComponent.Regex({body: tree});
+    return new RegexComponent.Regex({body: tree});
 }
 
 function Capture() {
-	const {tree, options} = parseArgs(arguments);
+    const {tree, options} = parseArgs(arguments);
 
-	return new RegexComponent.Capture({body: tree, name: options.as});
+    return new RegexComponent.Capture({body: tree, name: options.as});
 }
 
 function ChoiceOf() {
-	const {tree} = parseArgs(arguments);
+    const {tree} = parseArgs(arguments);
 
-	return new RegexComponent.ChoiceOf({body: tree});
+    return new RegexComponent.ChoiceOf({body: tree});
 }
 
 function OneOrMore() {
-	const {tree} = parseArgs(arguments);
+    const {tree} = parseArgs(arguments);
 
-	return new RegexComponent.OneOrMore({body: tree});
+    return new RegexComponent.OneOrMore({body: tree});
 }
 
 function Repeat() {
-	const {tree, options} = parseArgs(arguments);
+    const {tree, options} = parseArgs(arguments);
 
-	if(options.count && (options.from || options.to)) throw new Error("Repeat cannot have both count and from/to");
-	if(options.count < 0 || options.from < 0 || options.to < 0) throw new Error("Repeat count/from/to cannot be negative");
-	if(options.from && options.to && options.from > options.to) throw new Error("Repeat from cannot be greater than to");
-	if(options.count && options.count % 1 !== 0 || options.from && options.from % 1 !== 0 || options.to && options.to % 1 !== 0) throw new Error("Repeat count/from/to must be an integer");
+    if(options.count && (options.from || options.to)) throw new Error("Repeat cannot have both count and from/to");
+    if(options.count < 0 || options.from < 0 || options.to < 0) throw new Error("Repeat count/from/to cannot be negative");
+    if(options.from && options.to && options.from > options.to) throw new Error("Repeat from cannot be greater than to");
+    if(options.count && options.count % 1 !== 0 || options.from && options.from % 1 !== 0 || options.to && options.to % 1 !== 0) throw new Error("Repeat count/from/to must be an integer");
 
-	if(!options.from && options.to) options.from = 0;
+    if(!options.from && options.to) options.from = 0;
 
-	return new RegexComponent.Repeat({body: tree, isLazy: options.$reluctant, from: options.from, to: options.to, count: options.count});
+    return new RegexComponent.Repeat({body: tree, isLazy: options.$reluctant, from: options.from, to: options.to, count: options.count});
 }
 
 function One() {
-	const {tree} = parseArgs(arguments);
+    const {tree} = parseArgs(arguments);
 
-	return new RegexComponent.One({body: tree});
+    return new RegexComponent.One({body: tree});
 }
 
 function Optionally() {
-	const {tree, options} = parseArgs(arguments);
+    const {tree, options} = parseArgs(arguments);
 
-	return new RegexComponent.Optionally({body: tree, isLazy: options.$reluctant});
+    return new RegexComponent.Optionally({body: tree, isLazy: options.$reluctant});
 }
 
 function ZeroOrMore() {
-	const {tree, options} = parseArgs(arguments);
+    const {tree, options} = parseArgs(arguments);
 
-	return new RegexComponent.ZeroOrMore({body: tree, isLazy: options.$reluctant});
+    return new RegexComponent.ZeroOrMore({body: tree, isLazy: options.$reluctant});
 }
 
 
@@ -295,28 +301,28 @@ function ZeroOrMore() {
  * @return {{options: ObjectLiteral, tree: RegexComponent[]}}}} 
  */
 function parseArgs(args) {
-	const parsed = {
-		options: {},
-		tree: null
-	};
+    const parsed = {
+        options: {},
+        tree: null
+    };
 
-	// (options{})
-	if(Object.isObject(args[0])) parsed.options = args[0];
+    // (options{})
+    if(Object.isObject(args[0])) parsed.options = args[0];
 
-	// (tree[])
-	if(Array.isArray(args[0])) parsed.tree = args[0];
+    // (tree[])
+    if(Array.isArray(args[0])) parsed.tree = args[0];
 
-	// (options{}, tree[])
-	if(Array.isArray(args[1])) parsed.tree = args[1];
+    // (options{}, tree[])
+    if(Array.isArray(args[1])) parsed.tree = args[1];
 
-	// (...tree)
-	if(!Object.isObject(args[0]) && !Array.isArray(args[0])) parsed.tree = [...args];
+    // (...tree)
+    if(!Object.isObject(args[0]) && !Array.isArray(args[0])) parsed.tree = [...args];
 
-	// (options{}, ...tree)
-	if(!parsed.tree && args[1] && !Object.isObject(args[1]) && !Array.isArray(args[1])) parsed.tree = [...args].slice(1);
+    // (options{}, ...tree)
+    if(!parsed.tree && args[1] && !Object.isObject(args[1]) && !Array.isArray(args[1])) parsed.tree = [...args].slice(1);
 
 
-	return parsed;
+    return parsed;
 }
 
 /**
@@ -325,32 +331,32 @@ function parseArgs(args) {
  * @return {string} escaped string
  */
 function escapeRegexString(string) {
-	return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+    return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 }
 
 Object.defineProperty(Object, "isObject", {
-	value: function(obj) {
-		return obj && Object.getPrototypeOf(obj) === Object.prototype;
-	}
+    value: function(obj) {
+        return obj && Object.getPrototypeOf(obj) === Object.prototype;
+    }
 });
 
 Object.defineProperty(Object, "subclassOf", {
-	value: function(obj, con) {
-		return obj && (obj.prototype instanceof con || obj === con);
-	}
+    value: function(obj, con) {
+        return obj && (obj.prototype instanceof con || obj === con);
+    }
 });
 
 module.exports = {
-	Regex,
-	Capture,
-	ChoiceOf,
-	OneOrMore,
-	Repeat,
-	One,
-	Optionally,
-	ZeroOrMore,
-	$whitespace,
-	$digit,
-	$word,
-	RegexComponent
+    Regex,
+    Capture,
+    ChoiceOf,
+    OneOrMore,
+    Repeat,
+    One,
+    Optionally,
+    ZeroOrMore,
+    $whitespace,
+    $digit,
+    $word,
+    RegexComponent
 };
